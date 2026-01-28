5e9319b5919de1f5913792d40c9d63e141bb5a22
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2026-01-28 11:42:58 -0500

    Add files via upload

--- Backend/Routes/chatRoutes.js
+++ Backend/Routes/chatRoutes.js
@@ -3,15 +3,7 @@ import { chatWithPDF } from "../Controllers/qaChainControllers.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    // Passes user input to LangChain. Entire AI pipeline executes here
-    const answer = await chatWithPDF(req.body.question);
-    // Sends AI-generated answer to frontend
-    res.json({ answer });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// Chat Routes
+router.post("/", chatWithPDF);
 
 export default router;
